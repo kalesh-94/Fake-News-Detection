514e25453fb6f4c9416b9c0697df9f4395d38ed1
Kalesh Patil <159576620+kalesh-94@users.noreply.github.com>
2025-09-22 01:22:37 +0530

    Update App.jsx

--- frontend/fake-news-detector/src/App.jsx
+++ frontend/fake-news-detector/src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
     e.preventDefault()
     setLoading(true)
     try {
-      const res = await fetch('http://localhost:5000/api/predict', {
+      const res = await fetch('http://10.201.198.192:5000/api/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text: newsText }),
